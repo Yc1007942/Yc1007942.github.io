c4a4b631d0f1f1d9e6143418ba25637cd081d20f
Yc1007942 <145345309+Yc1007942@users.noreply.github.com>
2024-08-08 14:33:43 +0700

    Add files via upload

--- Final_project.py
+++ Final_project.py
@@ -1,6 +1,15 @@
-#CS50 Final Project
+import os, datetime
 from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
+app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+#test
+db = SQL("sqlite:///")
 
 
